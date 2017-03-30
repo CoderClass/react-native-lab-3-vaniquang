9f8a43558760c4a47548a083619e5477c95de7dd
Sub Admin <subadmin@Hoais-MacBook-Pro.local>
2017-03-30 20:52:11 +0700

    add map view

---
+++ apps/map.js
@@ -0,0 +1,160 @@
+
+
+import React, { Component } from 'react';
+import ImagePicker from 'react-native-image-picker';
+import Lightbox from 'react-native-lightbox';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput
+} from 'react-native';
+import MapView from 'react-native-maps';
+
+
+export default class Map extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialPosition: {
+        longitude: 0,
+        latitude: 0,
+      },
+      sourceImage: '',
+      positionArray: []
+    }
+  }
+  handleClickImage() {
+    ImagePicker.showImagePicker({
+      storageOptions: {
+        skipBackup: true, path: 'images'
+      }
+    }, (response) => {  
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };    // You can also display the image using data:    // let source = { uri: 'data:image/jpeg;base64,' + response.data };      this  .setState({
+        this.setState({
+          sourceImage: source
+        });
+      }
+
+    })
+  }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ initialPosition: position.coords });
+      },
+      (error) => alert(JSON.stringify(error)),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = JSON.stringify(position);
+      this.setState({ lastPosition });
+    });
+  }
+  render() {
+    console.log(this.state.initialPosition);
+    return (
+
+      <MapView
+        region={this.state.initialPosition}
+        style={{
+          flex: 1,
+        }}
+        onPress={
+          (e) => {
+            const { coordinate } = e.nativeEvent;
+            this.setState({ initialPosition: coordinate });
+            this.state.positionArray.push(coordinate);
+          }
+        }
+        onLongPress={(e) => {
+          this.handleClickImage();
+
+        }}
+      >
+        
+        {
+          this.state.positionArray.map(marker => (
+            <MapView.Marker
+              coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+              image = { require ('./flag.png')}
+              title={'Current position'}
+
+            >
+              <MapView.Callout
+                style={{
+                  alignItems: 'center', justifyContent: 'center',
+                }}
+              >
+
+                <Lightbox navigator={this.props.navigator}
+                  renderContent={() => {
+                    return (
+                      <Image source={this.state.sourceImage}
+                        style={{
+                          flex: 1
+                        }}
+                      />
+                    )
+                  }}
+                >
+                  <Image source={this.state.sourceImage}
+                    style={{
+                      width: 60, height: 60,
+                      margin: 4,
+                    }}
+                  />
+                </Lightbox>
+
+
+
+                <Text
+                  style={{
+                    fontSize: 12,
+                  }}
+                >
+                  Marker
+        </Text>
+              </MapView.Callout>
+            </MapView.Marker>
+
+          ))
+        }
+
+      </MapView>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
--- index.ios.js
+++ index.ios.js
@@ -1,79 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+
 
 import React, { Component } from 'react';
+import ImagePicker from 'react-native-image-picker';
+import Lightbox from 'react-native-lightbox';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TextInput
 } from 'react-native';
 import MapView from 'react-native-maps';
 
-const region = {
-  latitude: 10.8231,
-  longitude: 106.6297,
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421,
-}
-
+import Map from './apps/map';
 
 export default class PhotoMap extends Component {
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log('position', position);
-        var initialPosition = JSON.stringify(position);
-        this.setState({ initialPosition });
-      },
-      (error) => alert(JSON.stringify(error)),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-      var lastPosition = JSON.stringify(position);
-      this.setState({ lastPosition });
-    });
+  constructor(props) {
+    super(props);
+    this.state = { 
+      keyword : ''
+    }
+
   }
   render() {
+
     return (
-      <View style={styles.container}>
-        <MapView
-          region={region}
-          style={{
-            flex: 1,
-          }}
-        >
-          <MapView.Marker
-            coordinate=unspecified
-            title={'Current position'}
-          />
-        </MapView>
+      <View style={{flex :1 }} >
+        <TextInput style={{height : 50 }} />
+        <Map />
       </View>
+
+
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
 
 AppRegistry.registerComponent('PhotoMap', () => PhotoMap);
